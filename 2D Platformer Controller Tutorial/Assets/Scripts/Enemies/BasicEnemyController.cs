80725d736ab7eabf425e75022148ab0d7747efef
lenoadia <lenoadia@gmail.com>
2021-09-17 18:32:34 -0700

    Added comments to the script of the basic enemy.

--- 2D Platformer Controller Tutorial/Assets/Scripts/Enemies/BasicEnemyController.cs
+++ 2D Platformer Controller Tutorial/Assets/Scripts/Enemies/BasicEnemyController.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class BasicEnemyController : MonoBehaviour
 {
-    private enum State
+    private enum State // used as the state machine for the enemy
     {
         Moving,
         Knockback,
         Dead
     }
 
-    private State currentState;
+    private State currentState; // points to the current state of the enemy
 
     [SerializeField]
     private float
-        groundCheckDistance,
-        wallCheckDistance,
-        movementSpeed,
+        groundCheckDistance, // value used to detect if there is a ground in front of the enemy
+        wallCheckDistance, // value used to detect if there is a wall in front of the enemy
+        movementSpeed, // 
         maxHealth,
         knockbackDuration;
 
@@ -52,22 +52,23 @@ public class BasicEnemyController : MonoBehaviour
         groundDetected,
         wallDetected;
 
-    private GameObject alive;
-    private Rigidbody2D aliveRb;
-    private Animator aliveAnim;
+    private GameObject alive; // reference to the enemy's alive version's game object
+    private Rigidbody2D aliveRb; // reference to the enemy's alive version's rigid body component
+    private Animator aliveAnim; // reference to the enemy's alive version's animator component
 
     private void Start()
     {
-        alive = transform.Find("Alive").gameObject;
-        aliveRb = alive.GetComponent<Rigidbody2D>();
-        aliveAnim = alive.GetComponent<Animator>();
+        alive = transform.Find("Alive").gameObject; // gets a reference to the child game object named "Alive"
+        aliveRb = alive.GetComponent<Rigidbody2D>(); // gets a reference to the rigid body component of the alive game object
+        aliveAnim = alive.GetComponent<Animator>(); // gets a reference to the animator component of the alive game object
 
-        currentHealth = maxHealth;
-        facingDirection = 1;
+        currentHealth = maxHealth; // sets the current health of the enemy
+        facingDirection = 1; // sets the default facing of the enemy
     }
 
     private void Update()
     {
+        // updates the enemy according to its current state
         switch (currentState)
         {
             case State.Moving:
@@ -91,14 +92,14 @@ public class BasicEnemyController : MonoBehaviour
 
     private void UpdateMovingState()
     {
-        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-        wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround); // casts a ray from child "groundCheck" position down to check for ground
+        wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround); // casts a ray from child "wallCheck" position to the front of it to check for wall
 
-        if (!groundDetected || wallDetected)
+        if (!groundDetected || wallDetected) // if no ground was detected or a wall was detected
         {
             Flip();
         }
-        else
+        else // just continue moving on the current facing of the enemy
         {
             movement.Set(movementSpeed * facingDirection, aliveRb.velocity.y);
             aliveRb.velocity = movement;
@@ -114,14 +115,18 @@ public class BasicEnemyController : MonoBehaviour
 
     private void EnterKnockbackState()
     {
-        knockbackStartTime = Time.time;
+        knockbackStartTime = Time.time; // gets the current time
+
+        // pushes the enemy away from the attacking character
         movement.Set(knockbackSpeed.x * damageDirection, knockbackSpeed.y);
         aliveRb.velocity = movement;
-        aliveAnim.SetBool("knockback", true);
+
+        aliveAnim.SetBool("knockback", true); // sets the enemy's current animation to knockback
     }
 
     private void UpdateKnockbackState()
     {
+        // if the knockback duration expires
         if (Time.time >= knockbackStartTime + knockbackDuration)
         {
             SwitchState(State.Moving);
@@ -130,14 +135,14 @@ public class BasicEnemyController : MonoBehaviour
 
     private void ExitKnockbackState()
     {
-        aliveAnim.SetBool("knockback", false);
+        aliveAnim.SetBool("knockback", false); // sets the parameter "knockback" to false so that the animator component will switch to another specific animation
     }
 
     // Dead State -------------------------------------------
 
     private void EnterDeadState()
     {
-        // spawn chunks and blood
+        // spawn chunks and blood using the prefabs
         Instantiate(deathChunkParticle, alive.transform.position, deathChunkParticle.transform.rotation);
         Instantiate(deathBloodParticle, alive.transform.position, deathBloodParticle.transform.rotation);
         
@@ -158,11 +163,11 @@ public class BasicEnemyController : MonoBehaviour
 
     private void Damage(float[] attackDetails)
     {
-        currentHealth -= attackDetails[0];
+        currentHealth -= attackDetails[0]; // reduce the enemy's current health
 
-        Instantiate(hitParticle, alive.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+        Instantiate(hitParticle, alive.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f))); // create hit particles from the prefab
 
-        if (attackDetails[1] > alive.transform.position.x)
+        if (attackDetails[1] > alive.transform.position.x) // gets the source direction of the attack
         {
             damageDirection = -1;
         }
@@ -171,13 +176,11 @@ public class BasicEnemyController : MonoBehaviour
             damageDirection = 1;
         }
 
-        // hit particles
-
-        if (currentHealth > 0.0f)
+        if (currentHealth > 0.0f) // if the enemy is still alive, set it to its knockback state
         {
             SwitchState(State.Knockback);
         }
-        else if (currentHealth <= 0.0f)
+        else if (currentHealth <= 0.0f) // if the enemy is still alive, set it to its dead state
         {
             SwitchState(State.Dead);
         }
